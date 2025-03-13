10c3ecad437f8a1c6404b788a8d8e2cfcf9dfd44
LUXSHALINI <satheeskumarluxshalini@gmail.com>
2025-03-13 15:04:42 +0530

    project

--- index.js
+++ index.js
@@ -19,6 +19,12 @@ function calmultiply(){
 function caldivide() {
     let num1 = document.getElementById('number1').value;
     let num2 = document.getElementById('number2').value;
-    let total= Number(num1)/ Number(num2);
-    document.getElementById('result').innerHTML = total;
+    if (num2 == "0"){
+        document.getElementById('result').innerHTML= "Cannot divide byJavaScript Classwork 02 zero!";
+    }
+    else {
+        let total= Number(num1) / Number(num2);
+        document.getElementById('result').innerHTML = total;
+    }
 }
+
